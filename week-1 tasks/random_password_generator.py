68cc10e08e301d5fb77c3c3a475cedac02373735
Deepak Singla <78155202+dsingla2701@users.noreply.github.com>
2024-06-09 18:45:20 +0530

    Update random_password_generator.py
    Added a feature which tells the strength of the password.

--- week-1 tasks/random_password_generator.py
+++ week-1 tasks/random_password_generator.py
@@ -16,12 +16,24 @@ def generate_password(length, use_uppercase, use_lowercase, use_digits, use_spec
     if use_special:
         char_pool += string.punctuation
 
-    if length < 1:
-        raise ValueError("Password length must be at least 1")
-
+    if length < 4 :
+        raise ValueError("Password length must be at least 4")
+        
     password = ''.join(random.choice(char_pool) for _ in range(length))
     return password
 
+def check_password_strength(password, use_uppercase, use_lowercase, use_digits, use_special):
+        criteria_count = sum([use_uppercase, use_lowercase, use_digits, use_special])
+
+        if criteria_count == 1:
+            return "Very weak."
+        elif criteria_count == 2:
+            return "Weak."
+        elif criteria_count == 3:
+            return "Strong."
+        elif criteria_count == 4:
+            return "Very Strong."
+            
 def main():
     parser = argparse.ArgumentParser(description='Random Password Generator')
     parser.add_argument('-l', '--length', type=int, required=True, help='Length of the password')
@@ -40,7 +52,11 @@ def main():
             use_digits=args.digits,
             use_special=args.special
         )
+
+        strength = check_password_strength(password, args.uppercase, args.lowercase, args.digits, args.special)
         print(f'Generated random password is: {password}')
+        print(f'Strength of password is: {strength}')
+        
     except ValueError as e:
         print(f'Error: {e}')
 
